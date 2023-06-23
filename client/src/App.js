10ad9a7080d1f048f098f7e6eaad9f4b2db09970
rajkishorepatra <rpatrasm@gmail.com>
2023-06-24 00:58:43 +0530

    ..

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
-<<<<<<< HEAD
 import './App.css';
 // import JobListing from './pages/JobListing';
 import Landing from './pages/Landing';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import AllRoutes from "./AllRoutes";
+import Navbar from "./components/navbar";
 
 
 function App() {
   return (
     <>
       {/* <JobListing/> */}
-      <Landing />
-    </>
-=======
-import "./App.css";
-import AllRoutes from "./AllRoutes";
-import Navbar from "./components/navbar";
-
-function App() {
-  return (
-    <div className="App">
+      {/* <Landing /> */}
       <Navbar />
       <AllRoutes />
-    </div>
->>>>>>> origin/om
-  );
+    </>
+  )
 }
 
 export default App;
