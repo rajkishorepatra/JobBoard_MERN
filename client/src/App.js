cdca22b82b5521e33b79fa6ff39b38b2c17c99a0
ABH8S3K <abhisek26oct@gmail.com>
2023-06-24 10:45:32 +0530

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { Space } from "antd";
+import "./App.css";
+import AppFooter from "./Components/AppFooter";
+import AppHeader from "./Components/AppHeader";
+import PageContent from "./Components/PageContent";
+import SideMenu from "./Components/SideMenu";
 
 function App() {
   return (
-    <>This is app.js</>
+    <div className="App">
+      <AppHeader />
+      <div className="SideMenuAndPageContent">
+        <SideMenu></SideMenu>
+        <PageContent></PageContent>
+      </div>
+      <AppFooter />
+    </div>
   );
 }
-
 export default App;
